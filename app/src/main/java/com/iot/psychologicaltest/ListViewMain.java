057f9584f6bbf262d4b5b6a2d38d79a2c2a67cb6
안수정 <sj3672@naver.com>
2017-05-24 15:40:52 +0900

    arrays & listviewmain 수정

--- app/src/main/java/com/iot/psychologicaltest/ListViewMain.java
+++ app/src/main/java/com/iot/psychologicaltest/ListViewMain.java
@@ -8,13 +8,12 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-public class ListViewMain extends AppCompatActivity {
-//L
+import static com.iot.psychologicaltest.QuestionActivity.REQUEST_CODE_MENU;
 
+public class ListViewMain extends AppCompatActivity {
 
     private ListAdapter adapter;
     private ListView listView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,36 +23,33 @@ public class ListViewMain extends AppCompatActivity {
         adapter = new ListAdapter();
         listView = (ListView) findViewById(R.id.listView);
 
+        setData();
+        listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent=new Intent(getApplicationContext(),QuestionActivity.class);
-                intent.putExtra("data", listCustom.get(position));
-                startActivity(intent);
+                Intent intent = new Intent(getApplicationContext(), QuestionActivity.class);
+                Content content = new Content(position, 0);
+                intent.putExtra("content", content);
+                startActivityForResult(intent, REQUEST_CODE_MENU);
+                //startActivity(intent);
             }
         });
-
-        listView.setAdapter(adapter);
-
     }
 
-            // 보통 ListView는 통신을 통해 가져온 데이터를 보여줍니다.
-            // arrResId, titles, contents를 서버에서 가져온 데이터라고 생각하시면 됩니다.
-            private void setData() {
-                TypedArray arrResId = getResources().obtainTypedArray(R.array.resId);
-                String[] titles = getResources().getStringArray(R.array.title);
+    private void setData() {
+        TypedArray arrResId = getResources().obtainTypedArray(R.array.resId);
+        String[] titles = getResources().getStringArray(R.array.subtitle);
 
+        for (int i = 0; i < arrResId.length(); i++) {
+            ListItem dto = new ListItem();
+            dto.setResId(arrResId.getResourceId(i, 0));
+            dto.setTitle(titles[i]);
 
-                for (int i = 0; i < arrResId.length(); i++) {
-                    ListItem dto = new ListItem();
-                    dto.setResId(arrResId.getResourceId(i, 0));
-                    dto.setTitle(titles[i]);
-
-
-                    adapter.addItem(dto);
-                }
-            }
-
-
+            adapter.addItem(dto);
         }
+    }
+
+
+}
