b3924d136031b64d9bfa27757589cb1b5daa8b4d
gaeul <changge1004@naver.com>
2017-05-24 10:32:30 +0900

    리스트 화면 합체

---
+++ app/src/main/java/com/iot/psychologicaltest/ListItem.java
@@ -0,0 +1,24 @@
+package com.iot.psychologicaltest;
+
+public class ListItem {
+ private int resId;
+ private String title;
+
+
+ public int getResId() {
+  return resId;
+ }
+
+ public void setResId(int resId) {
+  this.resId = resId;
+ }
+
+ public String getTitle() {
+  return title;
+ }
+
+ public void setTitle(String title) {
+  this.title = title;
+ }
+
+}
