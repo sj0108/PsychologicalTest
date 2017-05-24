0039286cefae09b84068cb8e14319b610dd9ccc8
gaeul <changge1004@naver.com>
2017-05-24 15:22:39 +0900

    리스트

--- app/src/main/java/com/iot/psychologicaltest/ListViewMain.java
+++ app/src/main/java/com/iot/psychologicaltest/ListViewMain.java
@@ -1,14 +1,20 @@
 package com.iot.psychologicaltest;
 
+import android.content.Intent;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class ListViewMain extends AppCompatActivity {
 
+
+
     private ListAdapter adapter;
     private ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +25,18 @@ public class ListViewMain extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listView);
 
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent(getApplicationContext(),QuestionActivity.class);
+                intent.putExtra("data", listCustom.get(position));
+                startActivity(intent);
+            }
+        });
+
         listView.setAdapter(adapter);
+
     }
-
-
-
 
             // 보통 ListView는 통신을 통해 가져온 데이터를 보여줍니다.
             // arrResId, titles, contents를 서버에서 가져온 데이터라고 생각하시면 됩니다.
